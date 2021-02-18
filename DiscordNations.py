b254ece4a967d7aadf2f64601e2c949d2223e3ff
firez2469 <47616467+firez2469@users.noreply.github.com>
2021-02-17 23:18:27 -0500

    Delete DiscordNations.py

--- DiscordNations.py
+++
@@ -1,7 +0,0 @@
-class Nation:
-    def __init__(self, name, influence, leaderUser):
-        self.name = name
-        self.influence = influence
-        self.leaderUser = leaderUser
-    def addInfluence(self):
-        self.influence+=1
